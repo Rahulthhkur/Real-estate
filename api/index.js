b1d31f759c867638c6ec1dfada8e1b8edf7808a7
Rahul Singh <rthakur1233@gmail.com>
2024-04-03 14:46:14 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -1,54 +1,51 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-const userRouter = require('./routes/user.route.js');
-const authRouter = require('./routes/auth.routes.js');
-const listingRouter = require('./routes/lisiting.route.js');
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const path = require('path');
-const corsConfig ={
-    origin: "*",
-    credential: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
-app.options("", cors(corsConfig));
-app.use(cors());
-
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-const app= express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+  const __dirname = path.resolve();
 
-
-mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Failed to connect to MongoDB:', err);
-    });
-
-const _dirname = path.resolve();
+const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-// Error handling middleware
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
